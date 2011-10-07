51a7dbabd14a35db39c799655363ce99185d615b
cheremin <cheremin@gmail.com>
2011-10-07 10:27:52 +0000

    move to slf4j logging improved RetryerException to provide stack trace of first of wrapped exceptions

--- code/java/src/main/java/org/retryer/RetryerException.java
+++ code/java/src/main/java/org/retryer/RetryerException.java
@@ -3,6 +3,8 @@ package org.retryer;
 import java.util.Collections;
 import java.util.List;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
 /**
@@ -11,11 +13,15 @@ import com.google.common.collect.Lists;
  * @author cheremin
  * @since 03.08.11,  17:24
  */
-public final class RetryerException extends Exception{
+public final class RetryerException extends Exception {
     private final List<? extends Throwable> thrown;
 
     public RetryerException( final Iterable<? extends Throwable> thrown ) {
+        Preconditions.checkArgument( !Iterables.isEmpty( thrown ), "thrown can't be empty" );
+
         this.thrown = Lists.newArrayList( thrown );
+        final Throwable first = thrown.iterator().next();
+        initCause( first );
     }
 
     public List<? extends Throwable> getThrown() {
